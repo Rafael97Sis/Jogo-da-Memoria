4b20a54aac20695597aff9c09dcfce74be3f6cad
Rafael Rodrigues <33671644+Rafael97Sis@users.noreply.github.com>
2022-07-21 00:18:54 -0300

    update 0.1
    update 0.1

---
+++ Atividade-sintaxe_operadores/script.js
@@ -0,0 +1,13 @@
+var currentNumberSpam = document.getElementById("resultado");
+var valor01 = document.getElementById('valor01');
+var valor02 = document.getElementById('valor02');
+
+function verificar(valor01, valor02) {
+
+
+  if (condition) {
+
+  }
+
+
+}
